c71390284ed909b4f8937bcbb4467caac185e0fa
Jake Cooper <jake.elijah.cooper@gmail.com>
2024-12-24 08:05:10 +0000

    with latest count update

--- src/server.tsx
+++ src/server.tsx
@@ -245,6 +245,13 @@ const server = Bun.serve({
             }
           };
           
+          // Update local cache
+          latestCounts.set(REGION, {
+            region: REGION,
+            count: newValue,
+            lastUpdate: update.lastUpdate
+          });
+          
           // Send update to all connected WebSocket clients
           const wsMessage = JSON.stringify(update);
           clients.forEach(client => client.send(wsMessage));
