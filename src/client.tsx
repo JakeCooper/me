6f606f18ecf3f7e9028737f7efe50eafe253c80d
Jake Cooper <jake.elijah.cooper@gmail.com>
2024-12-24 18:58:35 +0000

    with tailwind

--- src/client.tsx
+++ src/client.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import { Counter } from "./components/Counter";
 import type { RegionData } from "./types";
+import './styles.css';
 
 declare global {
   interface Window {
