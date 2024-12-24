1ddd6e0a8ed76f6b86036367b7b80d825c57f4fb
Jake Cooper <jake.elijah.cooper@gmail.com>
2024-12-24 01:58:53 +0000

    oops png

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -80,7 +80,7 @@ function GlobeViz({ regions, currentRegion }: CounterProps) {
         ref={globeEl}
         width={800}
         height={800}
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-water.jpg"
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-water.png"
         bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
         
         pointsData={pointsData}
