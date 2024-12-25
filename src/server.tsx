83d80cf05864cd312c61471af6a8c2d4e3cb791c
Jake Cooper <jake.elijah.cooper@gmail.com>
2024-12-25 00:21:54 +0000

    remove the client cache buster

--- src/server.tsx
+++ src/server.tsx
@@ -233,7 +233,7 @@ const server = Bun.serve({
                 box-sizing: border-box;
               }
             </style>
-            <script src="/client.js?v=${Date.now()}" defer></script>
+            <script src="/client.js" defer></script>
           </head>
           <body>
             <div id="root">${content}</div>
